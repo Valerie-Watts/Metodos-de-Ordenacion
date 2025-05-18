e30f153450ba34ddc691df80b3449f5b1c3fd115
Valerie Watts <valerie.watts.feria@gmail.com>
2025-05-18 11:18:01 -0600

    Add project files.

---
+++ Ejercicios clase/Ordenación.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Métodos_de_Ordenación
+{
+    class Ordenación
+    {
+        public void print(int[] array)
+        {
+            foreach (int k in array)
+            {
+                Console.Write(k + " ");
+            }
+            Console.WriteLine();
+        } 
+
+        public void BurbujaMenor(int[] array)
+        {
+            int n = array.Length - 1;
+            int AUX; 
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = n; j >= i; j--)
+                {
+                    if (array[j-1] > array[j])
+                    {
+                        AUX = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = AUX;
+
+                        print(array);
+                    }
+                }
+            }
+        }
+
+        public void BurbujaMayor(int[] array)
+        {
+            int n = array.Length;
+            int AUX;
+
+            for (int i = (n - 1) ; i >= 1; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        AUX = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = AUX;
+
+                        print(array);
+                    }
+                }
+            }
+        }
+
+        public void BurbujaSeñal(int[] array) 
+        {
+            bool BAND = false;
+            int N = array.Length;
+            int i = 0;
+
+            while ((i < (N)) && (BAND == false))
+            {
+                BAND = true;
+                for (int j = 0; j < (N-1); j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+
+                        int AUX = array[j];
+                        array[j]= array[j + 1];
+                        array[j + 1] = AUX;
+                        BAND = false;
+                        print(array); 
+                    }
+
+                }
+                i = i + 1;  
+            }          
+        }
+         public void Sacudida(int[] array)
+        {
+            int IZQ = 1;
+            int DER = array.Length - 1;
+            int AUX;
+            int k = array.Length - 1;
+            while (DER >= IZQ)
+            {
+                for (int i = DER; i >= IZQ; i--)
+                {
+                    if (array[i - 1] > array[i])
+                    {
+                        AUX = array[i - 1];
+                        array[i - 1] = array[i];
+                        array[i] = AUX;
+                        k = i;
+
+                        print(array); 
+
+                    }
+                }
+                IZQ = k + 1;
+
+                for (int i = IZQ; i <= DER; i++)
+                {
+                    if (array[i - 1] > array[i])
+                    {
+                        AUX = array[i - 1];
+                        array[i - 1] = array[i];
+                        array[i] = AUX;
+                        k = i;
+                        print(array); 
+
+                    }
+                }
+                DER = k - 1;
+
+            }
+        }
+        
+        public void Inserción (int[] array)
+        {
+            int AUX;
+            int k = array.Length - 1;
+            int n = array.Length - 1;
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                AUX = array[i];
+                k = i - 1;
+
+                while ((k >= 1) && (AUX < array[k]))
+                {
+                    array[k + 1] = array[k];
+                    k = k - 1;
+                }
+                array[k + 1] = AUX;
+
+                print(array);
+            }
+        }
+
+        public void InserciónBinaria(int[] array)
+        {
+            int IZQ;
+            int DER;
+            int AUX, j, m;
+            int n = array.Length - 1;
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                AUX = array[i];
+                IZQ = 1;
+                DER = i - 1;
+
+                while (IZQ <= DER)
+                {
+                    m = (IZQ + DER) / 2;
+
+                    if (AUX <= array[m])
+                    {
+                        DER = m - 1;
+                    }
+                    else
+                    {
+                        IZQ = m + 1;
+                    }
+                }
+                j = i - 1;
+
+                while (j >= IZQ)
+                {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+                array[IZQ] = AUX;
+
+                print(array); 
+            }
+        }
+
+        public void Selección(int[] array)
+        {
+            int menor, k;
+            int n = array.Length;
+
+            for (int i = 0; i <= (n - 1); i++)
+            {
+                menor = array[i];
+                k = i;
+
+                for (int j = (i + 1); j < n; j++)
+                {
+                    if (array[j] < menor)
+                    {
+                        menor = array[j];
+                        k = j;
+                    }
+                }
+                array[k] = array[i];
+                array[i] = menor;
+
+                print(array); 
+            }
+        }
+
+        public void Shell(int[] array)
+        {
+            int i, AUX;
+            bool band;
+            int n = array.Length;
+            int INT = n + 1;
+
+
+            while (INT > 1)
+            {
+                INT = INT / 2;
+                band = true; 
+                
+                while (band == true)
+                {
+                    band = false;
+                    i = 0; 
+
+                    while ((i + INT) < n)
+                    {
+                        if (array[i] > array[i + INT])
+                        {
+                            AUX = array[i];
+                            array[i] = array[i + INT];
+                            array[i + INT] = AUX;
+                            band = true;
+                            print(array);
+
+                        }
+                        i += 1;
+
+                    }
+                    
+
+                }
+
+
+            }
+        }
+
+        public void quicksort_rapidoRecursivo(int[] array)
+        {
+            quicksort_reduceRecursivo(0, array.Length-1, array);
+        }
+
+        public void quicksort_reduceRecursivo(int INI, int FIN, int[] array)
+        {
+            int izq, der, pos, aux;
+            bool band;
+            
+            izq = INI;
+            der = FIN;
+            pos = INI;
+            band = true; 
+
+            while(band== true)
+            {
+                band = false;
+                while ((array[pos] <= array[der]) && (pos < der))
+                {
+                    der--; 
+                }
+                if (pos < der)
+                {
+                    aux = array[pos]; 
+                    array[pos] = array[der];
+                    array[der] = aux;
+                    pos = der; 
+
+                    while (array[pos] >= array[izq] && pos > izq)
+                    {
+                        izq++; 
+                    }
+
+                    if (pos > izq)
+                    {
+                        band = true;
+                        aux = array[pos];
+                        array[pos] = array[izq]; 
+                        array[izq] = aux;
+                        pos = izq;
+
+                        
+                    }
+
+                     
+                }
+                print(array);
+            }
+
+            if ((pos - 1) > INI)
+            {
+                quicksort_reduceRecursivo(INI,pos-1,array); 
+            }
+            if(FIN > (pos + 1))
+            {
+                quicksort_reduceRecursivo(pos + 1, FIN, array); 
+            }
+
+        }
+
+        public void heapsort_insert(int[] array)
+        {
+            int k, aux;
+            bool band; 
+
+            for(int i = 1; i < array.Length; i++)
+            {
+                k = i;
+                band = true; 
+
+                while (k>0 && band == true)
+                {
+                    band = false; 
+
+                    if (array[k] > array[k / 2])
+                    {
+                        aux = array[k / 2];
+                        array[k / 2] = array[k];
+                        array[k] = aux;
+                        k = k / 2;
+                        band = true; 
+                    }
+                    
+                }
+                print(array);
+            }
+        }
+
+        public void heapsort_delete(int[] array)
+        {
+            int aux, izq, der, k, ap;
+            bool BOOL; 
+
+            for (int i = array.Length - 1; i >= 1; i--)
+            {
+                aux = array[i]; 
+                array[i] = array[0];
+                izq = 1; 
+                der = 2; 
+                k = 0; 
+                BOOL = true; 
+
+                while (izq < i && BOOL == true)
+                {
+                    int mayor = array[izq];
+                    ap = izq;
+
+                    if (mayor < array[der] && der < i)
+                    {
+                        mayor = array[der];
+                        ap = der;
+                    }
+
+                    if (aux < mayor)
+                    {
+                        array[k] = array[ap];
+                        k = ap; 
+                    }
+                    else
+                    {
+                        BOOL = false; 
+                    }
+
+                    izq = k * 2; 
+                    der = izq + 1; 
+                }
+                array[k] = aux;
+                print(array);
+            }
+        }
+
+        public void heapsort(int[] array)
+        {
+            heapsort_insert(array);
+            heapsort_delete(array);            
+        }
+
+
+    }
+}
---
+++ Ejercicios clase/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Métodos_de_Ordenación
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ordenación metodos = new Ordenación();
+            int opt; 
+             
+
+            while (true)
+            {
+                int[] array = new int[] { 1, 4, 5, 7, 3, 8, 2, 9, 10, 21, 6, 11, 51, 49, 80, 100, 30, 64, 63, 2149, 552, 1864 };
+
+
+                Console.WriteLine("\nEste es el arreglo a ordenar: ");
+                metodos.print(array);
+                Console.WriteLine("\n------------------------------------------------");
+                Console.WriteLine("===Escoja el metodo para ordenar el arreglo===");
+                Console.WriteLine("Ordenamiento Burbuja: \n--1.Burbuja Menor\n--2.Burbuja Mayor\n--3.Burbuja Señal");
+                Console.WriteLine("4.Ordenamiento de Sacudida\nOrdenamiento de Inseción:\n--5.Inserción\n--6.Inserción Binaria");
+                Console.WriteLine("7.Ordenamiento por Selección\n8.Ordenamiento por Shell\n9.Ordenamiento por Quicksort\n10.Ordenamiento por Heapsort\n11.Salir");
+                Console.WriteLine("------------------------------------------------");
+                Console.Write("Escriba su opción: ");
+                opt = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(); 
+                
+                switch (opt)
+                {
+                    case 1:
+                        metodos.BurbujaMenor(array); 
+                        break;
+                    case 2:
+                        metodos.BurbujaMayor(array); 
+                        break;
+                    case 3:
+                        metodos.BurbujaSeñal(array); 
+                        break;
+                    case 4:
+                        metodos.Sacudida(array); 
+                        break;
+                    case 5:
+                        metodos.Inserción(array);
+                        break;
+                    case 6:
+                        metodos.InserciónBinaria(array);
+                        break;
+                    case 7:
+                        metodos.Selección(array);
+                        break;
+                    case 8:
+                        metodos.Shell(array);
+                        break;
+                    case 9:
+                        metodos.quicksort_rapidoRecursivo(array);
+                        break;
+                    case 10:
+                        metodos.heapsort(array); 
+                        break;
+                    case 11:
+                        Console.WriteLine("Adiós");
+                        return; 
+
+                }
+            }
+        }
+    }
+}
